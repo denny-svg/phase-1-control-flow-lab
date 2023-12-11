e4ebdcc286f9deba0e60524c207caf2996c985f1
denny-svg <pknowledge@gmail.com>
2023-12-11 18:43:03 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride<=400) {
+    return 'This one is on me!';
+  } else if(ride>2000){
+    if(ride>2500){
+      return'No can do.';
+    }
+    return 'I will gladly take your thirty bucks.';
+  }
+  else {
+    return 'I will gladly take your thirty bucks.';
+  }
 }
+scuberGreetingForFeet(199);
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+    
+  function ternaryCheckCity(city){
+    // Write your code here!
+    return city === "NYC"?  "Ok, sounds good.": "No go.";
+    
+  }
+  
+  function switchOnCharmFromTip(tip){
+    // Write your code here!
+    switch (tip) {
+      case "generous":
+       return 'Thank you so much.'
+        break;
+    case "not as generous": 
+    return "Thank you."
+        break;
+      default: 
+      return "Bye."
+        break;
+    }
+
+  }
+
